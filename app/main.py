3cdc030783116cb042cdbc9e6f673d1e84f49528
Shivkumar N <shivakumarn.anuthisoft.com>
2026-03-09 12:32:12 +0530

    fix flake8 formatting

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {"message": "CI/CD Demo"}
 
+
 @app.get("/add")
 def add(a: int, b: int):
     return {"result": a + b}
+
 
 @app.get("/multiply")
 def multiply(a: int, b: int):
